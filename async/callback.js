860e5dd48f432fc1e3a6ac6e711ff52a2efff0a1
raziel214 <soulreavers214@gmail.com>
2020-06-15 16:23:45 -0500

    Asynchronous Functions

---
+++ async/callback.js
@@ -0,0 +1,3 @@
+function imAsincrono() {
+    
+}
---
+++ conceptos/entorno.js
@@ -0,0 +1,7 @@
+let nombre = process.env.NOMBRE || 'how are you';
+let web = process.env.MI_WEB || 'i dont have web';
+
+console.log('Hello ' + nombre);
+console.log('My web pages is  ' + web);
+
+console.log('Other things')
