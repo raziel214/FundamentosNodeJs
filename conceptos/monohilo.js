2d006ed7f19ae7532b222227715fc6150334e309
raziel214 <soulreavers214@gmail.com>
2020-06-14 23:20:29 -0500

    validate  treads mono

---
+++ conceptos/monohilo.js
@@ -0,0 +1,14 @@
+console.log("Hola mundo");
+
+let i = 0;
+setInterval(function () { 
+    console.log(i);
+
+    if (i === 5) {
+        console.log("forzamos el error");
+        var a = 3 + z
+        
+    }
+    i++;
+
+},1000)
