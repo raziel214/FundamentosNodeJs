0c263f58e5c810feaeccd2904b1bbb162057dc4f
raziel214 <soulreavers214@gmail.com>
2020-06-15 21:16:08 -0500

    Asynchronous Functions step 2

--- async/callback.js
+++ async/callback.js
@@ -1,3 +1,29 @@
-function imAsincrono() {
+function imAsincrono(nombre,mycallback) {
+    
+    setTimeout(function () {
+        console.log('Hola' + nombre);
+        mycallback();
+        
+        
+    }, 1000);
     
 }
+function adios(nombre, mycallback) {
+
+    setTimeout(function () {
+        console.log('Adios' + nombre);
+        mycallback();
+
+
+    }, 1000);
+
+}
+
+console.log('......starting process');
+
+imAsincrono('John', function () {
+    adios('john', function () { 
+        console.log('......stop process');
+    })
+});
+
