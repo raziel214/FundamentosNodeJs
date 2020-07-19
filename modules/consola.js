61e74e7e34f237197d4c38ea55711ac3a6feb5e1
Raziel214 <soulreavers214@gmail.com>
2020-07-19 17:56:56 -0500

    console log

---
+++ modules/consola.js
@@ -0,0 +1,14 @@
+//console.log('Algo');
+var table = [
+    {
+        a: 1,
+        b:'z'
+    },
+    {
+        a: 2,
+        b:'otra cosa'
+
+    }
+]
+console.log(table);
+console.table(table);
