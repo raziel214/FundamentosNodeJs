71c38caac20132087c7e50f146b15011a7796fe0
Raziel214 <soulreavers214@gmail.com>
2020-07-18 23:21:39 -0500

    file system add, delete, read

--- modules/fs.js
+++ modules/fs.js
@@ -1,2 +1,29 @@
 const fs = require('fs');
 const util = require('util');
+
+const readFile = util.promisify(fs.readFile)
+
+function leer(ruta, cb) { 
+    fs.readFile(ruta, (err, data) => { 
+        cb(data.toString());
+    })
+
+}
+function escribir(ruta, contenido, cb) { 
+    fs.writeFile(ruta, contenido, function (err) {
+        if (err) {
+            console.error('No he podido escribir', err);
+        } else { 
+            console.log('Se ha escrito correctamente')
+        }
+
+     });
+}
+
+function borrar(ruta, cb) {
+    fs.unlink(ruta, cb);
+ }
+
+//escribir(__dirname + '/archivo1.txt', 'Soy un archivo nuevo jajajajajajajaja');
+//leer(__dirname+'/archivo.txt',console.log)
+borrar(__dirname + '/archivo1.txt', console.log);
