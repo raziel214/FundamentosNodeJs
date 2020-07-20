8c88da68793588be85efd9b1b71e2b2076b3b795
Raziel214 <soulreavers214@gmail.com>
2020-07-19 20:22:59 -0500

    Module c++ in nodejs

---
+++ modules/natives/build/Release/.deps/Release/obj.target/addon/hola.o.d
@@ -0,0 +1,17 @@
+cmd_Release/obj.target/addon/hola.o := c++ -o Release/obj.target/addon/hola.o ../hola.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node -I/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/src -I/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/deps/openssl/config -I/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/deps/openssl/openssl/include -I/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/deps/uv/include -I/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/deps/zlib -I/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/deps/v8/include  -O3 -gdwarf-2 -mmacosx-version-min=10.10 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/addon/hola.o.d.raw   -c
+Release/obj.target/addon/hola.o: ../hola.cc \
+  /Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/node.h \
+  /Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8.h \
+  /Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8-internal.h \
+  /Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8-version.h \
+  /Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8config.h \
+  /Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8-platform.h \
+  /Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/node_version.h
+../hola.cc:
+/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/node.h:
+/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8.h:
+/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8-internal.h:
+/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8-version.h:
+/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8config.h:
+/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/v8-platform.h:
+/Users/desarrollomp/Library/Caches/node-gyp/12.18.1/include/node/node_version.h:
