234ed3239a96524faccdff546e99a63cbd148253
Raziel214 <soulreavers214@gmail.com>
2020-07-19 21:41:55 -0500

    module os

---
+++ modules/os.js
@@ -0,0 +1,27 @@
+const os = require('os');
+console.log(os.platform);// plataforma
+console.log(os.hostname());//  Voy a saber el hostname de la máquina
+console.log(os.networkInterfaces());// Puedo acceder a mi interfaz de red activas en mi máquina, puedo saber  IPVX
+console.log(os.tmpdir());//-->Me muestra los directorios temporales, temproales una imagen que voy a procesar
+console.log(os.homedir()); // Me permite saber cual es el directorio raíz
+console.log(os.arch()); //----> Me devuelve la arquitecura de mi OS
+console.log(os.platform());//---> Me dice en qué plataforma estoy
+console.log(os.cpus());//--->podemos acceder a la información de las cpus de mi pc.
+console.log(os.constants);//--->  Me muestran todos los errores de sistema.
+
+
+//Acceder a espacios de memoria es muy útil para saber si tengo a memoria suficiente para realizar esta operación.
+const SIZE = 1024;
+function kb(bytes) { return bytes / SIZE }
+function mb(bytes) { return kb(bytes) / SIZE }
+function gb(bytes) { return mb(bytes) / SIZE }
+console.log(os.freemem());// ---> Me dice en bytes la memoria libre que tenemos
+console.log(kb(os.freemem()));
+console.log(mb(os.freemem()));
+console.log(gb(os.freemem()));
+console.log(gb(os.totalmem())); // ---> Me muestra la memoria disponible del pc.
+console.log('Hostname of any server')
+console.log(os.hostname())
+console.log(os.homedir())
+console.log(os.tmpdir())
+console.log('')
